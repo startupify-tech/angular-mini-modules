ca4588f92b25356699ef01a62623b7a489b5990e
Issue-03 <ishucuriosity@gmail.com>
2020-04-05 16:14:51 +0530

    adding notification feature

---
+++ notify-app/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NotificationComponent } from "./notification/notification.component";
+
+
+const routes: Routes = [
+  {path: "notifications", component: NotificationComponent, pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
