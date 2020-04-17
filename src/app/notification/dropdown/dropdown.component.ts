e6e5d227c6580fed0f548d600b32652ece9e8da1
Issue-03 <ishucuriosity@gmail.com>
2020-04-17 23:27:17 +0530

    polling in dropdown

--- src/app/notification/dropdown/dropdown.component.ts
+++ src/app/notification/dropdown/dropdown.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {interval} from "rxjs/internal/observable/interval";
+import {startWith, switchMap} from "rxjs/operators";
 
 import { NotificationService } from '../notification.service';
 import { Notification } from '../notification.model';
@@ -17,8 +19,11 @@ export class DropdownComponent implements OnInit {
   constructor(private http:HttpClient,private notificationService: NotificationService) {}
 
   ngOnInit(): void {
-    // this.notificationService.createNotifications()
-    this.notificationService.getNotifications().subscribe(
+    interval(5000)
+    .pipe(
+      startWith(0),
+      switchMap(  () => this.notificationService.getNotifications()  )
+    ).subscribe(
       notifications => {
         this.notifications = notifications;
       },
